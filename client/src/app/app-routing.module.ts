f97557cfb3e7fc6d77c082a73c3c05cf990be6f4
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-05-27 16:04:07 +0500

    login, register & reset layout designed

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { ResetComponent } from './pages/auth/reset/reset.component';
+import { AllEmployeesComponent } from './pages/employee/all-employees/all-employees.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'reset-password', component: ResetComponent},
+  {path: 'employees', component: AllEmployeesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,57 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { LoginComponent } from './pages/auth/login/login.component';
+// angular material imports
+import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+
+import { ResetComponent } from './pages/auth/reset/reset.component';
+import { AllEmployeesComponent } from './pages/employee/all-employees/all-employees.component';
+
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ResetComponent,
+    AllEmployeesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+
+    NzTableModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
