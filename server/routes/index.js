e5712d1f490dcc84df777ce446e740f1cb1a832d
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-06-01 15:28:24 +0500

    Update Routes (index), Admin & Employee all routes added.

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const router = express.Router()
 // ====
 const authController = require('../controllers').auth
 const adminController = require('../controllers').admin
+const employeeController = require('../controllers').employee
 
 // Welcome Check
 router.get('/', (req, res) => res.status(200).send({
@@ -18,14 +19,29 @@ router.get('/', (req, res) => res.status(200).send({
 
 // Login Route
 router.post('/login', authController.login)
-// >>> Admin Routes
-// Get All Admins Route
+
+// >>> Admins Routes
+// Get Admin By ID
+router.get('/admin/id/:id', adminController.getAdminById)
+// Get All Admins
 router.get('/admin/all', adminController.getAllAdmin)
-// Create Admin Route
+// Create Admin
 router.post('/admin/create', adminController.createAdmin)
-// Update Admin Route
+// Update Admin
 router.put('/admin/update/:id', adminController.updateAdmin)
-// Delete Admin Route
+// Delete Admin
 router.delete('/admin/delete/:id', adminController.deleteAdmin)
+
+// >>> Employees Routes
+// Get Employee By ID
+router.get('/employee/id/:id', employeeController.getEmployeeById)
+// Get All Employees
+router.get('/employee/all', employeeController.getAllEmployees)
+// Create Employee
+router.post('/employee/create', employeeController.createEmployee)
+// Update Employee
+router.put('/employee/update/:id', employeeController.updateEmployee)
+// Delete Employee
+router.delete('/employee/delete/:id', employeeController.deleteEmployee)
 
 module.exports = router;
