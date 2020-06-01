68689be3b37bdc2be4164ce8f12b9ea10ce9bdc6
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-06-01 15:29:23 +0500

    Controller Updated.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,11 @@
 // All Controllers
 const auth = require('./AuthController')
 const admin = require('./AdminController')
+const employee = require('./EmployeeController')
 
 // Export All Controllers
 module.exports = {
     auth,
     admin,
+    employee,
 }
