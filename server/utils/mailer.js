7a5497b863f91a8a876d3cdbf80ab27a7196869a
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-06-01 22:58:59 +0500

    Update mailer.js, mail configuration.

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -3,27 +3,53 @@ const nodemailer = require('nodemailer');
 
 module.exports = {
     send: function (email, subject, body) {
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
+
+        let transporter = nodemailer.createTransport({
+            pool: true,
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // use TLS
             auth: {
-                user: 'k152123@nu.edu.pk',
-                pass: 'cs495@li'
-            },
-            tls: {
-                rejectUnauthorized: false
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD
+            }
+        });
+        // var transporter = nodemailer.createTransport({
+        //     host: process.env.MAIL_SERVICE,
+        //     port: process.env.PORT,
+        //     secure: process.env.MAIL_ENCRYPTION_SSL, // use SSL
+        //     auth: {
+        //         user: process.env.MAIL_USERNAME,
+        //         pass: process.env.MAIL_PASSWORD
+        //     },
+        //     tls: {
+        //         rejectUnauthorized: false
+        //     }
+        // });
+
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Server is ready to take our messages");
             }
         });
 
         var mailOptions = {
-            from: 'k152123@nu.edu.pk',
+            from: process.env.MAIL_FROM_ADDRESS,
             to: email,
             subject: subject,
             text: body
         };
 
-        transporter.sendMail(mailOptions, function(error, info){
+        console.log(mailOptions);
+
+        // res.setHeader('Content-Type', 'text/plain');
+
+        transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
-                console.log("masla2 " + error);
+                console.log("Error From NODE_MAILER:  " + error);
                 return false;
             } else {
                 console.log('Email sent: ' + info.response);
