94bb0eff0bc65367d4c25ce110ed9f9491a756b5
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-06-02 14:20:43 +0500

    AttendanceController Created.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@
 const auth = require('./AuthController')
 const admin = require('./AdminController')
 const employee = require('./EmployeeController')
+const attendance = require('./AttendanceController')
 
 // Export All Controllers
 module.exports = {
     auth,
     admin,
     employee,
+    attendance,
 }
