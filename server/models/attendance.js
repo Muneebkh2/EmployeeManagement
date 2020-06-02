bbbd3c2cf39e90b3cb7652000fd3cebe0f2281f9
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-06-03 00:00:16 +0500

    Update Attendance model,

--- server/models/attendance.js
+++ server/models/attendance.js
@@ -1,16 +1,25 @@
+const models = require('../models/index')
+
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     const Attendance = sequelize.define('Attendance', {
-        user_id: DataTypes.INTEGER,
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: models.User,
+                key: 'id'
+            }
+        },
         day: DataTypes.DATE,
-        status: DataTypes.BOOLEAN
-    }, {});
+        status: DataTypes.BOOLEAN,
+
+    }, {underscored: true});
     Attendance.associate = function (models) {
-        Attendance.belongsTo(models.User, {
-            foreignKey: 'user_id',
-            as: 'users',
-            onDelete: 'CASCADE'
-        })
+        Attendance.belongsTo(models.User, { 
+            foreignKey: 'user_id', 
+            constraints: false,
+            as: 'users' 
+        });
     };
     return Attendance;
 };
